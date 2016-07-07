76e42e4602321df2afc103f4427354ceea4fdc8e
pjy <810256984@qq.com>
2016-07-08 02:15:24 +0800

    add GitDemo
    添加了一个工程

---
+++ GitDemo/GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by qi777 on 16/7/8.
+//  Copyright © 2016年 qi777. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitDemo/GitDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by qi777 on 16/7/8.
+//  Copyright © 2016年 qi777. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
